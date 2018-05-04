5f790c11a59756db7844972a50bf380dc30b779d
sungpil han <ttt16541@gmail.com>
2018-05-04 11:13:59 +0900

    모듈 통합

--- test/getimage.py
+++ test/getimage.py
@@ -8,10 +8,15 @@ import cv2
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from socket import *
+from threading import Thread
+import os
+import sys
 
 class image_converter:
   def __init__(self):
     self.bridge = CvBridge()
+    self.send_img = send_imgae()
     self.image_sub = rospy.Subscriber("/rgb/image",Image,self.callback)
 
   def callback(self,data):
@@ -20,8 +25,20 @@ class image_converter:
     except CvBridgeError as e:
       print(e)
 
+    self.send_img(cv_image)
+    
     cv2.imshow("Image window", cv_image)
     cv2.waitKey(3)
+
+class send_image:
+  def __init__(self):
+    self.s = socket(AF_INET, SOCK_STREAM)
+    self.s.bind(("",12224))
+    self.s.listen(10)
+    self.conn, addr = self.s.accept()
+
+  def send_img(img):
+    self.conn.send(img.tobytes())
 
 if __name__ == '__main__':
   ic = image_converter()
