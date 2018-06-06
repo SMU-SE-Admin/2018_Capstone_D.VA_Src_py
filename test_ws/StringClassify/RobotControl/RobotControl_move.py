f44d973253c231fd4be6d3641585c85069bc0a4a
sungpil han <ttt16541@gmail.com>
2018-06-07 01:02:05 +0900

    #1516 로봇 조종 테스팅
    소켓 오류 수정

--- test_ws/StringClassify/RobotControl/RobotControl_move.py
+++ test_ws/StringClassify/RobotControl/RobotControl_move.py
@@ -1,10 +1,19 @@
-import socket
+import socket, subprocess
 sock = socket.socket()
 sock.bind(('',5006))
 sock.listen(10)
+
+data2 = "RobotControl_base_stop.py"
+path = "python "+os.path.join(os.path.dirname(os.path.abspath(__file__)),data2)
+p = subprocess.Popen(path.split(' '))
+
 while True:
     cli, info = sock.accept()
-    data = cli.recv(1024)
-    data = data.split(":::")[0]
-    print data
+    data1 = cli.recv(1024)
+    data1 = data.split(":::")[0]
+    print data1
+    if data2 != data1:
+        path = "kill -9 "+str(p.pid)
+        path = "python "+os.path.join(os.path.dirname(os.path.abspath(__file__)),data1)
+        p = subprocess.Popen(path.split(' '))
     
