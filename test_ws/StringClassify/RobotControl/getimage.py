d4e339728477b9bf2b0ca1c826417a031420d4e7
sungpil han <ttt16541@gmail.com>
2018-06-07 21:35:26 +0900

    #1517 로봇 카메라 이미지 전송
    잘못된 변수 이름 수정
    sock -> self.sock
    ip -> self.ip
    port -> self.port

--- test_ws/StringClassify/RobotControl/getimage.py
+++ test_ws/StringClassify/RobotControl/getimage.py
@@ -9,6 +9,11 @@ import cv2
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+import socket
+import base64
+import numpy
+import time
+import os
 
 class image_converter:
 
@@ -18,23 +23,51 @@ class image_converter:
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/rgb/image",Image,self.callback)
 
+    pro = os.path.join(os.path.dirname(os.path.abspath(__file__)),"ip.txt")
+    f = open(pro, "r")
+    self.ip = f.readline()
+    f.close()
+    self.port = 5005
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
   def callback(self,data):
     try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
+    percent = 25
+    width = int(frame.shape[1] * percent / 100)
+    height = int(frame.shape[0] * percent / 100)
+    dim = (width, height)
+    frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
+    result, imgencode = cv2.imencode('.png', frame, encode_param)
+    data_img = numpy.array(imgencode)
+    row_img = data_img.tostring()
+    img = base64.standard_b64encode(row_img)
+    print(len(img))
+    s = str(len(img)) + ":::"
+    s = s.zfill(1024)
+    self.sock.sendto(s, (self.ip, self.port))
+    i = len(img)
+    send_size = 1024
+    offset = 0
+    while i > 0:
+        if i > send_size:
+            buf = img[offset: offset + send_size]
+            self.sock.sendto(buf, (self.ip, self.port))
+            #time.sleep(0.005)
+        else:
+            buf = img[offset: offset + i]
+            pend = 1024 - len(buf)
+            for i in range(0, pend):
+                buf = buf+"0"
+            #print len(buf)
+            self.sock.sendto(buf, (self.ip, self.port))
+            time.sleep(0.005)
+        offset += send_size
+        i -= send_size
 
-    (rows,cols,channels) = cv_image.shape
-    if cols > 60 and rows > 60 :
-      cv2.circle(cv_image, (50,50), 10, 255)
-
-    cv2.imshow("Image window", cv_image)
-    cv2.waitKey(3)
-
-    try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError as e:
-      print(e)
 
 def main(args):
   ic = image_converter()
